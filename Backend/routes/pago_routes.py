df3c15afb23e0139c15a06d25aa382ab2d3af038
kevinttbr2-cyber <kevinttbr2@gmail.com>
2026-07-14 21:09:02 -0400

    Update pago_routes.py

--- Backend/routes/pago_routes.py
+++ Backend/routes/pago_routes.py
@@ -14,6 +14,7 @@ from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch
 import pytz 
+import math
 
 # ============================
 # ZONA HORARIA CHILE
@@ -532,7 +533,7 @@ def crear_repuesto():
             iva = 1.19
             costo_con_iva = costo_proveedor * iva
             costo_venta_final = costo_con_iva * (1 + (margen_ganancia / 100))
-            costo_venta_final = round(costo_venta_final, 0)
+            costo_venta_final = int(costo_venta_final)
         
         costo_proveedor_pendiente = costo_proveedor == 0
         
@@ -579,7 +580,7 @@ def actualizar_repuesto(id_repuesto):
         iva = 1.19
         costo_con_iva = costo_proveedor * iva
         costo_venta_final = costo_con_iva * (1 + (margen_ganancia / 100))
-        costo_venta_final = round(costo_venta_final, 0)
+        costo_venta_final = math.floor(costo_venta_final)
         costo_proveedor_pendiente = costo_proveedor == 0
         
         conn = get_connection()
