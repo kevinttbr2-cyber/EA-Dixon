bd19a95eaf57e4c650545f8187c41180e76acaff
kevinttbr2-cyber <kevinttbr2@gmail.com>
2026-07-16 14:36:49 -0400

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -552,6 +552,5 @@ AuthService.crear_admin()
 # INICIAR
 # ============================
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
