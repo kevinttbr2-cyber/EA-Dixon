bbc7ecc5d17e9e74e1cb92fb92711aa4c0959777
kevinttbr2-cyber <kevinttbr2@gmail.com>
2026-07-19 19:56:54 -0400

    Include BaseRepository in repository exports
    Added BaseRepository to the exports of the module.

--- Backend/repositories/__init__.py
+++ Backend/repositories/__init__.py
@@ -1,4 +1,6 @@
-from repositories.usuario_repo import UsuarioRepository
-from repositories.pago_repo import PagoRepository
+# Backend/repositories/__init__.py
+from .usuario_repo import UsuarioRepository
+from .pago_repo import PagoRepository
+from .base_repository import BaseRepository
 
-__all__ = ['UsuarioRepository', 'PagoRepository']
+__all__ = ['UsuarioRepository', 'PagoRepository', 'BaseRepository']
