9106c884614e6e250011148995ca27f2ec3b4ea0
kevinttbr2-cyber <kevinttbr2@gmail.com>
2026-07-19 20:19:09 -0400

    Update __init__.py

--- Backend/utils/__init__.py
+++ Backend/utils/__init__.py
@@ -3,12 +3,15 @@ from .seguridad import *
 from .fecha_utils import *
 
 __all__ = [
+    # Seguridad
     'sanitizar_input', 'sanitizar_patente', 'sanitizar_numero',
     'sanitizar_booleano', 'sanitizar_dict', 'validar_filtro',
     'validar_metodo_pago', 'validar_estado', 'validar_fecha',
-    'validar_hora', 'generar_csrf', 'verificar_csrf',
+    'validar_hora', 'validar_email', 'validar_telefono',
+    'generar_csrf', 'verificar_csrf',
     'generar_firma_pdf', 'verificar_firma_pdf',
+    # Fechas
     'now_santiago', 'get_fecha_chile', 'get_hora_chile',
     'get_fecha_hora_chile', 'formatear_fecha', 'formatear_hora',
-    'formatear_fecha_espanol'
+    'formatear_fecha_espanol', 'calcular_dias_entre', 'sumar_dias'
 ]
